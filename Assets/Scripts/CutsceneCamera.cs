5a4981f86402f22d3ee3c6f85354d4fdba585622
Jared Trenholm <Jared.Trenholm12@gmail.com>
2021-09-23 13:35:24 -0300

    Updated Options for movement

--- Assets/Scripts/CutsceneCamera.cs
+++ Assets/Scripts/CutsceneCamera.cs
@@ -1,23 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CutsceneCamera : MonoBehaviour
 {
     public bool isInCutScene = false;
     public bool simpleMovementDebug = false;
+    public bool usingDefinedPos = false;
     public Vector3 cameraSpeeds;
+    public Vector3[] startPos;
+    public Vector3[] speeds;
     public GameObject cutSceneTarget;
     public float distanceToTravel;
     public float minSpeed;
     public float maxSpeed;
 
     private float distanceTraveled;
+    private int arrayIndex;
     void Start()
     {
         isInCutScene = true;
         distanceTraveled = 0;
-        RandomizeCameraSpeeds();
+        arrayIndex = -1;
+        DefineCameraPosAndSpeeds();
     }
 
     // Update is called once per frame
@@ -27,13 +33,29 @@ public class CutsceneCamera : MonoBehaviour
             CutSceneMovement();
     }
 
+    public void DebugMove()
+    {
+        simpleMovementDebug = true;
+        usingDefinedPos = false;
+    }
+    public void RandomMove()
+    {
+        simpleMovementDebug = false;
+        usingDefinedPos = false;
+    }
+    public void DefinedMove()
+    {
+        simpleMovementDebug = false;
+        usingDefinedPos = true;
+    }
+
     private void CutSceneMovement()
     {
         Vector3 previousPosition = this.transform.position;
         this.transform.Translate(cameraSpeeds*Time.deltaTime, Space.World);
         this.transform.LookAt(cutSceneTarget.transform);
         distanceTraveled += Vector3.Distance(previousPosition, this.transform.position);
-        if(Vector3.Distance(cutSceneTarget.transform.position, this.transform.position) <= 2f)
+        if(Vector3.Distance(cutSceneTarget.transform.position, this.transform.position) <= 3f)
         {
             distanceTraveled = distanceToTravel * 2;
         }
@@ -41,19 +63,29 @@ public class CutsceneCamera : MonoBehaviour
         {
             this.transform.position = new Vector3(cutSceneTarget.transform.position.x + Random.Range(-10, 10), cutSceneTarget.transform.position.y + Random.Range(0, 10), cutSceneTarget.transform.position.z + Random.Range(-10, 10));
             distanceTraveled = 0;
-            RandomizeCameraSpeeds();
+            DefineCameraPosAndSpeeds();
         }
     }
 
-    private void RandomizeCameraSpeeds()
+    private void DefineCameraPosAndSpeeds()
     {
-        if (!simpleMovementDebug)
+        if (!simpleMovementDebug && !usingDefinedPos)
         {
             cameraSpeeds = new Vector3(Random.Range(minSpeed, maxSpeed), Random.Range(minSpeed, maxSpeed), Random.Range(minSpeed, maxSpeed));
             if (cameraSpeeds.y <= 0)
             {
                 cameraSpeeds.y = 0;
             }
+        }
+
+        if (usingDefinedPos)
+        {
+            arrayIndex += 1;
+            if ((arrayIndex >= startPos.Length) || (arrayIndex >= speeds.Length))
+                arrayIndex = 0;
+
+            this.transform.position = startPos[arrayIndex];
+            cameraSpeeds = speeds[arrayIndex];
         }
     }
 }
